a0063207f436bf1e62a451b9cc31585eda2c43bf
Umid <umid-berdiev82@mail.ru>
2020-10-01 01:24:46 +0500

    About page updated

--- resources/js/pages/About.vue
+++ resources/js/pages/About.vue
@@ -1,5 +1,99 @@
 <template>
-  <div style="min-height: 50vh;">
-    <h1 class="m-5">Page is under construction!</h1>
+  <div id="about">
+    <b-container fluid class="p-md-5">
+      <b-card-group deck class="m-0">
+        <b-card class="border-0 mr-0">
+          <b-card-header class="bg-transparent px-0 mb-md-5">
+            <h2>Shortly About Us</h2>
+          </b-card-header>
+          <b-card-text class="font-weight-bold">PRAESENT VESTIBULUM MOLESTIE LACUS CONSECTETUER ADIPISCING ELIT.</b-card-text>
+          <b-card-text>Aenean nonummy hendrerit mauris. Phasellus porta. Fusce suscipit varius mi. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nulla dui. Fusce feugiat malesuada odio. Morbi nunc odio, gravida at, cursus nec, luctus a, lorem. Maecenas tristique orci ac sem. Duis ultricies pharetra magna.</b-card-text>
+          <b-card-text>Donec accumsan malesuada orci. Donec sit amet eros. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Mauris fermentum dictum magna.</b-card-text>
+          <b-button>MORE</b-button>
+        </b-card>
+        <b-card class="border-0 ml-0">
+          <b-card-header class="bg-transparent px-0 mb-md-5">
+            <h2>Ideas & Solutions</h2>
+          </b-card-header>
+          <b-card-text class="font-weight-bold">CUM SOCIIS NATOQUE PENATIBUS ET MAGNIS DIS PARTURIENT</b-card-text>
+          <b-card-text>Donec sit amet eros. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Mauris fermentum dictum magna. Sed laoreet aliquam leo. Ut tellus dolor, dapibus eget, elementum vel, cursus eleifend, elit. Aenean auctor wisi et urna. Aliquam erat volutpat. Duis ac turpis. Integer rutrum ante eu lacus.</b-card-text>
+          <ul class="item_list">
+            <li class="firstItem"><a href="#">Aenean auctor wisi et urna aliquam erat volutpat duis ac turpis</a></li>
+            <li><a href="#">Integer rutrum ante eu lacus lorem ipsum dolor sit amet</a></li>
+            <li class="lastItem"><a href="#">Consectetuer adipiscing elit praesent vestibulum molestie lac</a></li>
+          </ul>
+          <b-card-text class="font-weight-bold">PRAESENT VESTIBULUM MOLESTIE LACUS AENEAN NONUMMY</b-card-text>
+          <b-card-text>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Praesent vestibulum molestie lacus. Aenean nonummy hendrerit mauris. Phasellus porta. Fusce suscipit varius mi. Cum sociis natoque penatibus et magnis dis parturient montes.</b-card-text>
+          <b-card-text>Nascetur ridiculus mus. Nulla dui. Fusce feugiat malesuada odio. Morbi nunc odio, gravida at, cursus nec, luctus a, lorem. Maecenas tristique orci ac sem.</b-card-text>
+          <b-card-text class="font-weight-bold">DUIS ULTRICIES PHARETRA MAGNA. DONEC ACCUMSAN MALESUADA ORCI.</b-card-text>
+          <b-card-text>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Praesent vestibulum molestie lacus. Aenean nonummy hendrerit mauris. Phasellus porta. Fusce suscipit varius mi. Cum sociis natoque penatibus et magnis dis parturient montes.</b-card-text>  
+        </b-card>
+      </b-card-group>
+    </b-container>
+    <b-container fluid style="background: #f0f0f0;" class="p-md-5">
+      <b-card class="border-0 mx-md-3" bg-variant="transparent">
+        <b-card-header class="bg-transparent px-0 mb-md-5">
+          <h2>Exploring Textile Innovations</h2>
+        </b-card-header>
+        <b-card-text class="font-weight-bold">LOREM IPSUM DOLOR SIT AMET, CONSECTETUER ADIPISCING ELIT. PRAESENT VESTIBULUM MOLESTIE LACUS.</b-card-text>
+        <b-card-text>Donec sit amet eros. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Mauris fermentum dictum magna. Sed laoreet aliquam leo. Ut tellus dolor, dapibus eget, elementum vel, cursus eleifend, elit. Aenean auctor wisi et urna. Aliquam erat volutpat. Duis ac turpis. Integer rutrum ante eu lacus. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Praesent vestibulum molestie lacus. Aenean nonummy hendrerit mauris. Phasellus porta. Fusce suscipit varius mi.</b-card-text>
+        <b-card-text>Aenean auctor wisi et urna. Aliquam erat volutpat. Duis ac turpis. Integer rutrum ante eu lacus. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Praesent vestibulum molestie lacus. Aenean nonummy hendrerit mauris. Phasellus porta. Fusce suscipit varius mi. Cum sociis natoque penatibus et magnis.</b-card-text>
+        <b-card-text>Dis parturient montes, nascetur ridiculus mus. Nulla dui. Fusce feugiat malesuada odio. Morbi nunc odio, gravida at, cursus nec, luctus a, lorem. Maecenas tristique orci ac sem. Duis ultricies pharetra magna. Donec accumsan malesuada orci. Donec sit amet eros. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Mauris fermentum dictum magna. Sed laoreet aliquam leo. Ut tellus dolor, dapibus eget.</b-card-text>
+        <b-card-text>Elementum vel, cursus eleifend, elit. Aenean auctor wisi et urna. Aliquam erat volutpat. Duis ac turpis. Integer rutrum ante eu lacus. Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</b-card-text>
+        <b-button>MORE</b-button>
+      </b-card>
+    </b-container>
+    <b-container fluid class="p-md-5">
+      <b-card class="border-0 mx-md-3">
+        <b-card-header class="bg-transparent px-0 mb-md-3">
+          <h2>Our Approach to Business</h2>
+        </b-card-header>
+        <b-card-group deck>
+          <b-card class="border-0 mx-0">
+            <b-card-title class="h6">
+              <a href="#" class="text-decoration-none">CUM SOCIIS NATOQUE PENATIBUS ET MAGNIS DIS PARTURIENT</a>
+            </b-card-title>
+            <b-card-text>
+              Sed laoreet aliquam leo. Ut tellus dolor, dapibus eget, elementum vel, cursus eleifend, elit. Aenean auctor wisi et urna. Aliquam erat volutpat. Duis ac turpis. Integer rutrum ante eu lacus. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Praesent vestibulum molestie lacus.
+            </b-card-text>
+          </b-card>
+
+          <b-card class="border-0 mx-0">
+            <b-card-title class="h6">
+              <a href="#" class="text-decoration-none">LOREM IPSUM DOLOR SIT AMET, CONSECTETUER ADIPISCING ELIT</a>
+            </b-card-title>
+            <b-card-text>
+              Aliquam erat volutpat. Duis ac turpis. Integer rutrum ante eu lacus. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Praesent vestibulum molestie lacus. Aenean nonummy hendrerit mauris. Phasellus porta. Fusce suscipit varius mi. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nulla dui. Fusce feugiat malesuada odio.
+            </b-card-text>
+          </b-card>
+        </b-card-group>
+        <b-card-group deck>
+          <b-card class="border-0 mx-0">
+            <b-card-title class="h6">
+              <a href="#" class="text-decoration-none">PRAESENT VESTIBULUM MOLESTIE LACUS HENDRERIT MA</a>
+            </b-card-title>
+            <b-card-text>
+              Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Praesent vestibulum molestie lacus. Aenean nonummy hendrerit mauris. Phasellus porta. Fusce suscipit varius mi. Cum sociis natoque penatibus et magnis dis.
+            </b-card-text>
+            <b-card-text>
+              Parturient montes, nascetur ridiculus mus. Nulla dui. Fusce feugiat malesuada odio. Morbi nunc odio, gravida at, cursus nec, luctus a, lorem.
+            </b-card-text>
+          </b-card>
+
+          <b-card class="border-0 mx-0">
+            <b-card-title class="h6">
+              <a href="#" class="text-decoration-none">MORBI NUNC ODIO, GRAVIDA AT, CURSUS NEC, LUCTUS A, LOREM</a>
+            </b-card-title>
+            <b-card-text>
+              Fusce suscipit varius mi. Cum sociis natoque penatibus et magnis dis parturi ent montes, nascetur ridiculus mus. Nulla dui.
+            </b-card-text>
+            <b-card-text>
+              Fusce feugiat malesuada odio. Morbi nunc odio, gravida at, cursus nec, luctus a, lorem. Maecenas tristique orci ac sem. Duis ultricies pharetra magna. Donec accumsan malesuada orci. Donec sit amet eros.
+            </b-card-text>
+          </b-card>
+        </b-card-group>
+        
+      </b-card>
+    </b-container>
   </div>
 </template>
